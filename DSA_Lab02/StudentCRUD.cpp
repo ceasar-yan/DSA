0d5ef876de27553c2b3816c05744457854eb35b6
ceasar-yan <eduave.ceasarryan@jhcsc.edu.ph>
2026-09-22 20:04:30 +0800

    feat: basic activity implementation

--- DSA_Lab02/StudentCRUD.cpp
+++ DSA_Lab02/StudentCRUD.cpp
@@ -1,6 +1,36 @@
+#include <cstddef>
+#include <cstdlib>
 #include <print>
+#include <iostream>
+
+using namespace std;
+
+struct Student {
+  int id {};
+  string name {};
+  string course {};
+  int yearLevel {};
+};
+
+// Function Declaration
+void addStudent();
+void displayStudents();
+void updateStudent();
+void deleteStudent();
 
 int main() {
+  const size_t MAX_SIZE {5};
+  Student student[MAX_SIZE];
+  int currentCount {0};
+
+  bool exit {false};
+  system("clear");
+
+  do {
+    system("echo -e \"\e[34m$(figlet 'Student CRUD')\033[0m\"");
+  
+    exit = true;
+  } while (!exit);
 
   return 0;
 }
